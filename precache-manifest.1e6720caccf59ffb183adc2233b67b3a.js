877219b5d57fa82a734c2720a8cd3754bbbde452
AjitShiva <ajitshiva007@gmail.com>
2020-08-15 19:44:48 +0530

    Updates

---
+++ precache-manifest.1e6720caccf59ffb183adc2233b67b3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5b57cee2e94c1390da7899504692559",
+    "url": "/COVID-19-Live-Tracker/index.html"
+  },
+  {
+    "revision": "fb458551c1af32279330",
+    "url": "/COVID-19-Live-Tracker/static/css/main.f0f325c8.chunk.css"
+  },
+  {
+    "revision": "35ac7a2d26c076d79208",
+    "url": "/COVID-19-Live-Tracker/static/js/2.7d55f3cb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-Live-Tracker/static/js/2.7d55f3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb458551c1af32279330",
+    "url": "/COVID-19-Live-Tracker/static/js/main.e62c2630.chunk.js"
+  },
+  {
+    "revision": "aa43a03659afdd13b117",
+    "url": "/COVID-19-Live-Tracker/static/js/runtime-main.ee0f257c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Live-Tracker/static/media/image.d7265326.png"
+  }
+]);
